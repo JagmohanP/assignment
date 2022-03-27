0a3c46d0401b9fe654b4bc3ef0ac784d71dd34b8
JagmohanP <jagmohan.purohit@gmail.com>
2022-03-27 05:30:25 +0300

    - OpenAPI enabled for SwaggerUI for REST API documentation. REST APIs can be tried out easily on swagger ui

--- country-service-app/country-service/src/main/java/com/assignment/config/AppConfiguration.java
+++ country-service-app/country-service/src/main/java/com/assignment/config/AppConfiguration.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.assignment.exception.RestTemplateResponseErrorHandler;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
 public class AppConfiguration implements WebMvcConfigurer {
 
@@ -32,4 +36,21 @@ public class AppConfiguration implements WebMvcConfigurer {
         return restTemplate;
     }
 
+    /**
+     * Bean configuration for swagger UI
+     *
+     * @return bean with additional details to be shown on swagger UI
+     */
+    @Bean
+    public OpenAPI countryServiceOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("Country Service API")
+                                .description("Application to provide various details of a country like code, capital, population")
+                                .version("1.0-SNAPSHOT"))
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("External site used to fetch country information")
+                                .url("https://restcountries.com/"));
+    }
 }
